ce598465693cedacf1618ae584bb1d91101dd72e
Damian Poddebniak <poddebniak@mailbox.org>
2025-11-19 12:58:10 +0100

    build(nix): add flake.{nix,lock}

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05-small";
+  };
+
+  outputs =
+    { self, nixpkgs, ... }:
+    let
+      eachSupportedSystem = nixpkgs.lib.genAttrs supportedSystems;
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      mkDevShells =
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            strictDeps = true;
+            nativeBuildInputs = with pkgs; [
+              der-ascii
+              just
+              mkcert
+              rustPlatform.bindgenHook
+              rustup
+              zlint
+            ];
+          };
+        };
+    in
+    {
+      devShells = eachSupportedSystem mkDevShells;
+    };
+}
